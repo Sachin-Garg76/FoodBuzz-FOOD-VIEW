44f0d25072025c761ff87dbcbe2e23767b81fc65
Sachin Garg <s32314800@gmail.com>
2026-07-18 15:00:55 +0530

    Update MongoDB connection URI

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -1,14 +1,12 @@
 const mongoose = require("mongoose");
-
 function connectDB() {
   mongoose
-    .connect(process.env.MONGODB_URI)
+    .connect(process.env.Your_MONGODB_URI)
     .then(() => {
-      console.log("mongodb connect");
+      console.log("mongodb connected ");
     })
     .catch((err) => {
       console.log("Error : ", err);
     })
 }
-
 module.exports = connectDB;
